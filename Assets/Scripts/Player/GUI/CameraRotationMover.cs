9722fa85fe7fac08848a55815276108326e18e1c
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-01-06 21:54:07 +0500

    feat(Camera): implemented camera rotator

---
+++ Assets/Scripts/Player/GUI/CameraRotationMover.cs
@@ -0,0 +1,25 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace Player.GUI
+{
+    public class CameraRotationMover : MonoBehaviour
+    {
+        private CinemachineVirtualCamera _cinemachineVirtualCamera;
+
+        private void Awake()
+        {
+            _cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+        }
+        
+        public void Rotate(float angle, Vector3 direction)
+        {
+            if (direction == Vector3.zero)
+            {
+                return;
+            }
+            
+            _cinemachineVirtualCamera.transform.Rotate(direction, angle);
+        }
+    }
+}
