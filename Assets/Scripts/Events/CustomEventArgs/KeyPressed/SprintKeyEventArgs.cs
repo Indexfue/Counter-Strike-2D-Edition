2c7fbe82ccfb7cce4067af24d4d53c4ef3392ce9
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-01-15 22:08:10 +0500

    feat(Player): implemented sprint

---
+++ Assets/Scripts/Events/CustomEventArgs/KeyPressed/SprintKeyEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class SprintKeyEventArgs : CancelEventArgs
+    {
+        public SprintKeyEventArgs(GameObject sender, bool cancel = false) : base(sender, cancel) { }
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using Components;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -8,7 +9,9 @@ namespace Player
     [RequireComponent(typeof(CharacterController))]
     public sealed class PlayerMovement : MonoBehaviour
     {
-        [SerializeField, Range(0f, 1f)] private float baseMovementSpeed;
+        [SerializeField] private StaminaComponent staminaComponent;
+        
+        private float _currentMovementSpeed;
         private Vector3 _movementDirection;
         private Rigidbody2D _rigidbody;
         private CharacterController _characterController;
@@ -16,24 +19,27 @@ namespace Player
 
         private Coroutine _unitCounterCoroutine;
 
-        public float BaseMovementSpeed => baseMovementSpeed;
-        public float CurrentMovementSpeed => Math.Max(Math.Abs(_unitVector.XSpeedRate), Math.Abs(_unitVector.YSpeedRate));
+        public float CurrentMovementSpeed => _currentMovementSpeed;
+        public float CurrentUnitSpeed => Math.Max(Math.Abs(_unitVector.XSpeedRate), Math.Abs(_unitVector.YSpeedRate));
         public Vector3 MovementDirection => _movementDirection;
 
         private void Start()
         {
             _characterController = GetComponent<CharacterController>();
+            _currentMovementSpeed = Configuration.BaseMovementSpeed;
         }
 
         private void OnEnable()
         {
             EventManager.Subscribe<MovementKeyPressedEventArgs>(OnMovement);
+            EventManager.Subscribe<SprintKeyEventArgs>(OnSprintKeyPressed);
             _unitCounterCoroutine = StartCoroutine(UnitCounterRoutine());
         }
 
         private void OnDisable()
         {
             EventManager.Unsubscribe<MovementKeyPressedEventArgs>(OnMovement);
+            EventManager.Unsubscribe<SprintKeyEventArgs>(OnSprintKeyPressed);
             StopCoroutine(_unitCounterCoroutine);
         }
 
@@ -53,10 +59,57 @@ namespace Player
 
         private void OnMovement(MovementKeyPressedEventArgs args) => _movementDirection = args.MovementDirection.normalized;
 
+        private void OnSprintKeyPressed(SprintKeyEventArgs args)
+        {
+            if (!args.Cancel)
+            {
+                //HandleDash();
+
+                if (_unitVector.YSpeedRate == 0 && _unitVector.XSpeedRate == 0)
+                {
+                    return;
+                }
+
+                _currentMovementSpeed = Configuration.BaseSprintSpeed;
+                staminaComponent.HandleStaminaReduce();
+            }
+
+        }
+
+        private void OnSprintKeyUnpressed(SprintKeyEventArgs args)
+        {
+            if (args.Cancel)
+            {
+                _currentMovementSpeed = Configuration.BaseMovementSpeed;
+                staminaComponent.StopStaminaReduce();
+            }
+        }
+
+        private void HandleDash()
+        {
+            if (staminaComponent.CurrentStamina > Configuration.DashStaminaReduce)
+            {
+                if (_movementDirection == Vector3.zero)
+                {
+                    _movementDirection = transform.TransformDirection(Vector3.forward);
+                }
+                staminaComponent.CurrentStamina -= Configuration.DashStaminaReduce;
+                StartCoroutine(HandleDashRoutine());
+                _movementDirection = Vector3.zero;
+            }
+        }
+
+        private IEnumerator HandleDashRoutine()
+        {
+            _currentMovementSpeed = Configuration.BaseDashSpeed;
+            yield return new WaitForSeconds(0.2f);
+            _currentMovementSpeed = Configuration.BaseMovementSpeed;
+        }
+
         private void MoveCharacter()
         {
             var direction = (Vector3.right * _unitVector.XSpeedRate) + (Vector3.forward * _unitVector.YSpeedRate);
-            _characterController.Move(direction * baseMovementSpeed);
+            _characterController.Move(direction * _currentMovementSpeed);
         }
     }
 
