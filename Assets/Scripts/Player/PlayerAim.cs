d80023cabe7de9460b649db443fba96d8133adeb
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-01-18 19:20:02 +0500

    feat(Player): implemented aim feature

---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Player.PlayerEvents;
+using Player.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerAim : MonoBehaviour
+    {
+        [SerializeField] private AimView viewPrefab;
+        [SerializeField] private GameObject aimObjectPrefab;
+        [SerializeField] private LayerMask aimMask;
+        // To debug
+        [SerializeField] private Transform toFollow;
+
+        private GameObject _aimObject;
+        private Canvas _canvas;
+        
+        private void Start() => Initialize();
+        
+        private void OnEnable()
+        {
+            EventManager.Subscribe<SecondaryFireKeyEventArgs>(OnSecondaryFireKeyPressed);
+            EventManager.Subscribe<SecondaryFireKeyEventArgs>(OnSecondaryFireKeyUnpressed);
+        }
+
+        private void OnDisable()
+        {
+            EventManager.Unsubscribe<SecondaryFireKeyEventArgs>(OnSecondaryFireKeyPressed);
+            EventManager.Unsubscribe<SecondaryFireKeyEventArgs>(OnSecondaryFireKeyUnpressed);
+        }
+
+        private void Update()
+        {
+            UpdateAimObjectPosition();
+        }
+
+        private void OnSecondaryFireKeyPressed(SecondaryFireKeyEventArgs args)
+        {
+            if (args.Cancel)
+            {
+                return;
+            }
+
+            _aimObject = Instantiate(aimObjectPrefab, transform, true);
+            
+            EventManager.RaiseEvent(new CameraFollowEventArgs(gameObject, _aimObject.transform));
+            EventManager.RaiseEvent(new CameraZoomEventArgs(gameObject, Configuration.AimCameraZoomOrthoSize, 0.5f));
+        }
+
+        private void OnSecondaryFireKeyUnpressed(SecondaryFireKeyEventArgs args)
+        {
+            if (!args.Cancel)
+            {
+                return;
+            }
+
+            Destroy(_aimObject);
+            EventManager.RaiseEvent(new CameraFollowEventArgs(gameObject, transform));
+            EventManager.RaiseEvent(new CameraZoomEventArgs(gameObject, Configuration.BaseCameraZoomOrthoSize, 0.5f));
+        }
+
+        private void UpdateAimObjectPosition()
+        {
+            if (_aimObject == null)
+            {
+                return;
+            }
+            
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f, aimMask))
+            {
+                if (Vector3.Distance(hit.point, transform.position) < Configuration.AimRadius)
+                {
+                    _aimObject.transform.position = hit.point;
+                }
+            }
+        }
+
+        private void Initialize()
+        {
+            // canvas? ui?
+        }
+
+#if UNITY_EDITOR
+
+        private void OnDrawGizmos()
+        {
+            if (_aimObject != null)
+            {
+                Gizmos.DrawWireSphere(transform.position, Configuration.AimRadius);
+            }
+        }
+
+#endif
+    }
+}
---
+++ Assets/Scripts/Player/UI/AimView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player.UI
+{
+    public class AimView : MonoBehaviour
+    {
+        //TODO: Rewrite this
+        [SerializeField] private float lineWidth;
+
+        public float _thetaScale = 0.01f;
+        private float _theta;
+        private float _sightRadius = 3f;
+        
+        private int _size;
+        private LineRenderer _lineDrawer;
+        
+        public bool IsEnabled { get; set; }
+
+        private void Start()
+        {
+            _lineDrawer = GetComponent<LineRenderer>();
+        }
+
+        [Obsolete("Obsolete")]
+        private void LateUpdate()
+        {
+            UpdateView();
+        }
+
+        [Obsolete("Obsolete")]
+        private void UpdateView()
+        {
+            _theta = 0f;
+            _size = (int)((1f / _thetaScale) + 1f);
+            _lineDrawer.SetVertexCount(_size);
+            
+            for (int i = 0; i < _size; i++)
+            {
+                _theta += (2.0f * Mathf.PI * _thetaScale);
+                float x = _sightRadius * Mathf.Cos(_theta);
+                float y = _sightRadius * Mathf.Sin(_theta);
+                _lineDrawer.SetPosition(i, new Vector3(x, y, 0));
+            }
+        }
+    }
+}
