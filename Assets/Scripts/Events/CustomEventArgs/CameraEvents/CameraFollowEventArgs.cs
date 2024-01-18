1136eef65cb87a8625b4fd163a15cb8719e218d5
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-01-18 19:18:40 +0500

    feat(Event): added new camera events

---
+++ Assets/Scripts/Events/CustomEventArgs/CameraEvents/CameraFollowEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class CameraFollowEventArgs : BaseEventArgs
+    {
+        public Transform ToFollow { get; }
+        
+        public CameraFollowEventArgs(GameObject sender, Transform toFollow) : base(sender)
+        {
+            ToFollow = toFollow;
+        }
+    }
+}
---
+++ Assets/Scripts/Events/CustomEventArgs/CameraEvents/CameraZoomEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class CameraZoomEventArgs : BaseEventArgs
+    {
+        public float EndCameraOrthoSize { get; }
+        public float ZoomTime { get; }
+        
+        public CameraZoomEventArgs(GameObject sender, float endCameraOrthoSize, float zoomTime) : base(sender)
+        {
+            EndCameraOrthoSize = endCameraOrthoSize;
+            ZoomTime = zoomTime;
+        }
+    }
+}
